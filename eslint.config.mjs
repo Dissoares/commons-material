9f8c24fc5ccf2692fbc9ddedef79c507c8a4c26a
@dissoares <diegosoaresyt@gmail.com>
2026-03-04 16:03:33 -0300

    adicionando configuração do ESLint para TypeScript e Angular

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'out-tsc/**', 'coverage/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/component-class-suffix': [
+        'error',
+        {
+          suffixes: ['Component'],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended],
+    rules: {},
+  },
+);
