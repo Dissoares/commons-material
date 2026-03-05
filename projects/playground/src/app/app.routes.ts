749f374f72352bda0407cf5b5a7cdca692999b45
@dissoares <diegosoaresyt@gmail.com>
2026-03-05 14:06:44 -0300

    removendo componentes não utilizados e simplificando a estrutura do layout

--- projects/playground/src/app/app.routes.ts
+++ projects/playground/src/app/app.routes.ts
@@ -1,3 +1,23 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsComponent } from './forms/forms.component';
+import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'forms',
+    component: FormsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
--- projects/playground/src/app/app.ts
+++ projects/playground/src/app/app.ts
@@ -1,13 +1,9 @@
-import {
-  PageSectionComponent,
-  ThemeToggleComponent,
-  ToolbarComponent,
-} from '@dissoares/commons-material';
+import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [PageSectionComponent, ToolbarComponent, ThemeToggleComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
